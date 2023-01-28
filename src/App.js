5d972d4aaf816bf31774a7f42bf31cdd8b6babe2
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2023-01-28 16:40:12 -0600

    Created new Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header">
+          {" "}
+          <img src={logo} className="App-logo img-fluid" alt="logo" />{" "}
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
